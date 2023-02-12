502a2ada8b8bfc16cf93e6c3d0ade149d0e6599c
crgold <crgold@asu.edu>
2023-02-12 20:33:27 +0800

    init

---
+++ scripts/deploy.ts
@@ -0,0 +1,18 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const initBalance = 1;
+
+  const Assessment = await ethers.getContractFactory("Assessment");
+  const assessment = await Assessment.deploy(initBalance);
+  await assessment.deployed();
+
+  console.log(`A contract with balance of ${initBalance} eth deployed to ${assessment.address}`);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
